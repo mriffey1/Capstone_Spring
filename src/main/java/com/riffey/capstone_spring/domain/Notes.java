914f34918844434b42e0357647c1fb76e51310c8
MeganRiffey <mriffe4@wgu.edu>
2023-02-14 20:33:10 -0500

    Added Animals and animal list. Still attempting to figure out join tables with JPA. Modified the CSS to make the content more readable.

--- src/main/java/com/riffey/capstone_spring/domain/Notes.java
+++ src/main/java/com/riffey/capstone_spring/domain/Notes.java
@@ -1,4 +1,54 @@
 package com.riffey.capstone_spring.domain;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notes")
 public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notes_id")
+    private int notesId;
+
+    @Column(name = "Notes")
+    private String notesField;
+
+    @Column(name = "created")
+    private LocalDateTime noteCreated;
+
+    public int getNotesId() {
+        return notesId;
+    }
+
+    public void setNotesId(int notesId) {
+        this.notesId = notesId;
+    }
+
+    public String getNotesField() {
+        return notesField;
+    }
+
+    public void setNotesField(String notesField) {
+        this.notesField = notesField;
+    }
+
+    public LocalDateTime getNoteCreated() {
+        return noteCreated;
+    }
+
+    public void setNoteCreated(LocalDateTime noteCreated) {
+        this.noteCreated = noteCreated;
+    }
+
+    public int getAnimalId() {
+        return animalId;
+    }
+
+    public void setAnimalId(int animalId) {
+        this.animalId = animalId;
+    }
+
+    @Column(name = "animal_id")
+    private int animalId;
 }
